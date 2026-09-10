b02c87351e9bdbf77027b7bccc363328ede27d5e
tkdahmed33-arch <tkd.ahmed33@gmail.com>
2026-09-10 08:34:16 +0300

    Update HR_PROXY_URL to published API proxy URL

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 /* Set this to the published URL of the Replit API proxy before publishing on GitHub Pages. */
-window.HR_PROXY_URL = "/api/hr-proxy";
+window.HR_PROXY_URL = "https://script.google.com/macros/s/AKfycbxSEvTrINR0exv_e9RRksgGsv7c_imOIl546inlm8El9oieK_iSNfiJDv-EzkgjfBiH/exec/api/hr-proxy";
